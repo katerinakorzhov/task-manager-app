0ff0f96a7749d4905717d3c71f838476839fdb32
Katerina Korzhov <kkorzho@my.wgu.edu>
2026-03-14 15:11:45 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      //list group component that loads: //individual task components
+    </>
+  );
+}
+
+export default App;
