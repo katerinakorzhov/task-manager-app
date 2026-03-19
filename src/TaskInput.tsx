f3cc5467c3d600c3130fbba50375c5161d207cd8
Katerina Korzhov <kkorzho@my.wgu.edu>
2026-03-18 23:54:54 -0700

    Add TaskInput component

---
+++ src/TaskInput.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+// const [task, setTask] = useState("");
+
+function TaskInput() {
+  return <label>{/* Add a task: <input type="text" value={task} /> */}</label>;
+}
+
+export default TaskInput;
